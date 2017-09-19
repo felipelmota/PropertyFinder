9223a8636f3eb9af5ac84988363a49bd97cc4ab5
Felipe Mota <felipelmota@gmail.com>
2017-09-19 00:04:35 -0300

    PoC: using nestoria api and react native.

---
+++ index.ios.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NavigatorIOS,  
+  View
+} from 'react-native';
+
+import SearchPage from './SearchPage';
+
+class PropertyFinder extends Component {
+  render() {
+    console.log('SearchPage.render');
+    
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+  container: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
